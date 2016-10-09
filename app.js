752c0a21f11226281e8c7a2a0598b9fd259a4ccd
Waldemar Bira <waldek.bira@gmail.com>
2016-10-09 22:40:25 +0200

    Added connection with LUIS model

--- app.js
+++ app.js
@@ -3,8 +3,24 @@ var config = require('./config.json'),
     connector = new botbuilder.ConsoleConnector().listen(),
     bot = new botbuilder.UniversalBot(connector),
     recognizer = new botbuilder.LuisRecognizer(config.luisModelUrl),
-    dialog = new botbuilder.IntentDialog({ recognizers = [recognizer]});
-    
+    dialog = new botbuilder.IntentDialog({ recognizers: [recognizer] });
 
+
+bot.dialog('/', dialog);
+dialog.matches('FindBrewery', [
+    function (session, args, next) {
+        var brewery = botbuilder.EntityRecognizer.findEntity(args.entities, 'Brewery');
+        console.log(args);        
+    }
+]);
+
+dialog.matches('FindBeer', [
+    function (session, args, next) {
+        var beer = botbuilder.EntityRecognizer.findEntity(args.entities, 'Beer');
+        console.log(args);
+    }
+]);
+
+dialog.onDefault(botbuilder.DialogAction.send("Sorry, I didn't understand you."));
 
 
