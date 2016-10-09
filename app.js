88a275a5337d2c70d87974aa701d9cb4f7c847a3
Waldemar Bira <waldek.bira@gmail.com>
2016-10-09 20:52:43 +0200

    created basic app initialization

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
-var botbuilder = require('botbuilder'),
+var config = require('./config.json'),
+    botbuilder = require('botbuilder'),
     connector = new botbuilder.ConsoleConnector().listen(),
-    bot = new botbuilder.UniversalBot(connector);
+    bot = new botbuilder.UniversalBot(connector),
+    recognizer = new botbuilder.LuisRecognizer(config.luisModelUrl),
+    dialog = new botbuilder.IntentDialog({ recognizers = [recognizer]});
+    
+
 
 
